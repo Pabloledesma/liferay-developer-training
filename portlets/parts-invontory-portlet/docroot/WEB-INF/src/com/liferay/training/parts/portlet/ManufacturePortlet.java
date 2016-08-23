d015a49a15abe54201fc96b5d4421d3f42aadf16
= <=>
2016-08-23 10:28:24 -0500

    se crearon las entidades manufactorer y part

---
+++ portlets/parts-invontory-portlet/docroot/WEB-INF/src/com/liferay/training/parts/portlet/ManufacturePortlet.java
@@ -0,0 +1,11 @@
+package com.liferay.training.parts.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class ManufacturePortlet
+ */
+public class ManufacturePortlet extends MVCPortlet {
+ 
+
+}
